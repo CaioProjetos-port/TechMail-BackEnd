6e2f1f8f2f3ad8de8c5df5adbe7cf43dd1b407de
Caio Monte <caiomm076@gmail.com>
2025-06-19 14:33:51 -0300

    ajuste porta para Render

--- app.py
+++ app.py
@@ -39,3 +39,7 @@ def listar_usuarios():
 @app.route("/")
 def home():
     return "API funcionando"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
